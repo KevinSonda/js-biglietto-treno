c156c19171c7a8ac45db13d9648db5358c7d368a
KevinSonda <kevinsonda@gmail.com>
2024-07-01 06:07:57 +0200

    [UPD]Aggiunta soluzione primi 3 punti del problema

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Realizzare una variabile alla quale assegno un promtp per chiedere i km da percorrere
+let km = prompt('Inserisci i km da precorrere')
+
+//Realizzare una variabile alla quale assegno un promtp per chiedere l'eta del passeggero
+let eta = prompt('Inserisci la tua eta')
+
+//realizzio una variabile per contenere il prezzo base
+let prezzo_base = km * 0.21;
+
+//Dichiaro una variabile per il prezzo finale
+let prezzo_finale;
+
