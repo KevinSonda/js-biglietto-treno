f767cf669a1ba72a46674ae0b3e08f0a2db1b20b
KevinSonda <kevinsonda@gmail.com>
2024-07-01 06:11:30 +0200

    [UPD]Aggiunta condizione eta inferiore 18

--- js/script.js
+++ js/script.js
@@ -10,3 +10,12 @@ let prezzo_base = km * 0.21;
 //Dichiaro una variabile per il prezzo finale
 let prezzo_finale;
 
+//se l'eta e inferiore a 18 => applico uno sconto del 20%
+if (eta < 18) {
+    //Calcolo dal prezzo base lo sconto
+    let sconto = prezzo_base * 0.2;
+    //calcolo il prezzo finale
+    prezzo_finale = prezzo_base - sconto;
+}
+
+console.log(prezzo_finale)
